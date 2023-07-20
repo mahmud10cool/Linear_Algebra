97d62eaf775c6585d76f148755df17a1f1c244c2
mahmud10cool <mutashi@gmail.com>
2023-07-21 04:18:23 +0600

    Done with one quiz

--- lin_alg_1.py
+++ lin_alg_1.py
@@ -1,3 +1,18 @@
 # Proving the Cauchy-Schwarz inequality.
 # Condition for equality is that the two vectors are exactly equal (identical).
 
+import numpy as np
+
+# create three vectors, one of which is dependent on another
+a = np.random.randn(5)
+b = np.random.randn(5)
+c = np.random.randn(1) * a
+
+# computer their dot products
+aTb = np.dot(a,b)
+aTc = np.dot(a,c)
+
+# demonstrate the (in)equalities
+print(f'{np.abs(aTb):.4f}, {np.linalg.norm(a)*np.linalg.norm(b):.4f}')
+print(f'{np.abs(aTc):.4f}, {np.linalg.norm(a)*np.linalg.norm(c):.4f}')
+
